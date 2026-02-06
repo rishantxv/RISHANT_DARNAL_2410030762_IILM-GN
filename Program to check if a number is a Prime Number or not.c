166b9212d42eee970feb7d8f48372798aedfd716
rishantxv <rishantxv@gmail.com>
2026-02-07 03:11:07 +0530

    Update Program to check if a number is a Prime Number or not.c

--- Program to check if a number is a Prime Number or not.c
+++ Program to check if a number is a Prime Number or not.c
@@ -7,16 +7,11 @@ int main(){
     for (int i=2;i<=(N/2);i++){
         if(N%i==0){
             c+=1;
-        }
-        break;
+            break;
+        }        
     }if(c==0){
         printf("The no. is Prime");
     }else{
         printf("The no is not Prime");
     }
-
-
-
-
-
 }
